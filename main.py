2a0c7e25c461c5eee4740539fcb724e110c4943c
385ahmet <Ahmet16011601@icloud.com>
2025-11-18 21:36:25 +0300

    Add usage instructions and description comments
    Added comments explaining the purpose and usage of the script.

--- main.py
+++ main.py
@@ -91,3 +91,10 @@ info_ekran.pack(pady=20)
 pencere.mainloop()
 #made by ahmet :3
 #u can change everthing
+# Pc-status-tester
+#A "Pc status tester" it tests wifi disk etc. u can use to check ans system fault and tells how to fix it
+#To download:
+#fırst clone this repo
+#after installed
+#go to command bar type: pip clone psuitl
+#and now you can run this repo
